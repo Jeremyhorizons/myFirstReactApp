e96f7ffda206d9fd17f5e1c968ca725bb5c4005b
jchuprograms <jchuprograms@gmail.com>
2017-07-02 22:58:44 -0700

    Organizing React Code: Presentational/Container Components

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,34 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <form className="inputline">
+        <input type="text"/>
+        <input type="submit" value="Add todo item"/>
+      </form>
+    )
+  }
+}
 
 class Todo extends React.Component {
   constructor(props) {
     super(props);
   }
   render(){
+    // var isComplete = function(task) {
+    //   if (task) {
+    //     return {'text-decoration': 'line-through'};
+    //   }
+    // }
+
+    var isCompleted = function(task) {
+      if (task.completed) {
+        return <li><button type="button">X</button><strike>{task.taskText}</strike></li>
+      } else {
+        return <li><button type="button">X</button>{task.taskText}</li>
+      }
+    }
+
     return (
-      <div>
-        <li><button type="button">X</button>{" "}{this.props.task}</li>
-      </div>
+      <ul>
+        {isCompleted(this.props.task)}
+      </ul>
     )
   }
 }
 
-
 class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render(){
     return(
-      <div>
+      <div className="todolist">
         <h3>Todo List</h3>
         <ul>
-          {this.props.dummyData.map((task,i)=><Todo task={task}/>)}
+          {this.props.todos.map((task)=><Todo task={task}/>)}
         </ul>
       </div>
     )
   }
 }
-var dummyData = ["Jeremy is here", "Timmy is there", "Amy is everywhere", "David is no where"];
 
-ReactDOM.render(<ToDoList dummyData={dummyData} />,
+class ToDoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState ({
+      todos:todos
+    })
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <InputLine  />
+        <ToDoList todos={this.state.todos} />
+      </div>
+    )
+  }
+}
+
+var todos = [
+  {
+    taskText: "Jeremy Task 1",
+    completed: true
+  },
+  {
+    taskText: "David Task 1",
+    completed: false
+  },
+  {
+    taskText: "Timmy Task 1",
+    completed: false
+  }
+]
+
+
+ReactDOM.render(<ToDoApp todos={todos} />,
    document.getElementById('root'));
